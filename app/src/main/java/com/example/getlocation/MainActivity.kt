701b1b508e38756a9741c2c4c4ac46ab3b29dddb
tsuyoshi007 <hunvikran18@kit.edu.kh>
2019-12-08 11:29:34 +0700

    :sparkles: getting Azimuth (degree to the north)

--- app/src/main/java/com/example/getlocation/MainActivity.kt
+++ app/src/main/java/com/example/getlocation/MainActivity.kt
@@ -3,12 +3,18 @@ package com.example.getlocation
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,12 +22,14 @@ import org.json.JSONObject
 
 
 class MainActivity : AppCompatActivity() {
-
-    var longitude:Double? = 0.0
-    var latitude:Double? = 0.0
+    var longitude:String = ""
+    var latitude:String = ""
+    var mAzimuth:Double = 0.0
+    var sendBoolean:Boolean = false
     lateinit var ip:String
     lateinit var port:String
 
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -32,25 +40,20 @@ class MainActivity : AppCompatActivity() {
         }
 
         val lm = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-
         val locationListener = object: LocationListener {
             override fun onLocationChanged(location: Location?) {
-                longitude = location?.longitude
-                latitude = location?.latitude
+                longitude = location?.longitude.toString()
+                latitude = location?.latitude.toString()
 
-                longitudeView.text = longitude.toString()
-                latitudeView.text = latitude.toString()
-
-
-                val json = JSONObject()
-                json.put("longitude",longitude)
-                json.put("latitude",latitude)
-                val message = json.toString()
-
-
-                val messageSender = MessageSender(this@MainActivity)
-                messageSender.execute(ip,port,message)
-
+                longitudeView.text = longitude
+                latitudeView.text = latitude
+                if(sendBoolean) {
+                    val json = JSONObject()
+                    json.put("longitude",longitude)
+                    json.put("latitude",latitude)
+                    val message = json.toString()
+                    MessageSender().execute(ip,port,message)
+                }
             }
 
             override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
@@ -65,20 +68,55 @@ class MainActivity : AppCompatActivity() {
 
             }
         }
-        getCoordinateBtn.setOnClickListener {
+
+        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val rotationVectorSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)
+
+
+        val rvListener = object : SensorEventListener {
+            val orientation = FloatArray(3)
+            val rMat = FloatArray(9)
+            override fun onSensorChanged(event: SensorEvent?){
+                if (event!!.sensor.type == Sensor.TYPE_ROTATION_VECTOR) {
+                    SensorManager.getRotationMatrixFromVector(rMat, event.values)
+                    mAzimuth = (Math.toDegrees(SensorManager.getOrientation(rMat,orientation)[0].toDouble()) + 360) % 360
+                }
+                directionView.text= mAzimuth.toString()
+                if(sendBoolean) {
+                    val json = JSONObject()
+                    json.put("Azimuth",mAzimuth)
+                    val message = json.toString()
+                    MessageSender().execute(ip,port,message)
+                }
+            }
+            override fun onAccuracyChanged(sensor: Sensor, i: Int) {}
+        }
+
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,500,0f,locationListener)
+        sensorManager.registerListener(
+            rvListener,
+            rotationVectorSensor, SensorManager.SENSOR_DELAY_FASTEST
+        )
+
+
+        sendDataBtn.setOnClickListener {
             ip = ipEditText.text.toString()
             port = portEditText.text.toString()
-            if(ip.isNotEmpty() && port.isNotEmpty()){
-                lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,500,0f,locationListener)
+            if(ip.isEmpty() || port.isEmpty()){
+                Toast.makeText(applicationContext,"Please fill in the blank",Toast.LENGTH_LONG).show()
+            }else{
+                sendMessage()
             }
         }
 
+    }
+    fun sendMessage() {
+        sendBoolean = true
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         when (requestCode) {
              0-> {
-                // If request is cancelled, the result arrays are empty.
                 if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
                     Toast.makeText(this,"Granted",Toast.LENGTH_SHORT).show()
                 } else {
--- app/src/main/java/com/example/getlocation/MessageSender.kt
+++ app/src/main/java/com/example/getlocation/MessageSender.kt
@@ -1,14 +1,10 @@
 package com.example.getlocation
 
-import android.content.Context
 import android.os.AsyncTask
-import android.widget.Toast
 import java.io.DataOutputStream
-import java.lang.ref.WeakReference
 import java.net.Socket
 
-class MessageSender(context: Context):AsyncTask<String,Int,Boolean>(){
-    private val mContext: WeakReference<Context>  = WeakReference(context)
+class MessageSender:AsyncTask<String,Int,Boolean>(){
 
 
     override fun doInBackground(vararg p0: String?): Boolean? {
@@ -16,12 +12,12 @@ class MessageSender(context: Context):AsyncTask<String,Int,Boolean>(){
         val port = p0[1]!!
         try {
             val s = Socket(ip, port.toInt())
+            s.tcpNoDelay = true
             val dataOut = DataOutputStream(s.getOutputStream())
             dataOut.writeUTF(p0[2]!!)
             dataOut.flush()
             dataOut.close()
         }catch (e:Exception){
-            Toast.makeText(mContext.get(),"Error", Toast.LENGTH_SHORT).show()
         }
         return true
     }
